e90fbbbbd149ae3d6ac5bfaec913d6ca1f7a49b4
Manish suthar <manishsuthar78775@gmail.com>
2025-04-15 17:26:02 +0530

    Update script.js
    navbar functionality

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let hero=document.getElementById('hero');
+let hero = document.getElementById("hero");
+let nav_bar = document.querySelector(".nav_list");
+console.log(nav_bar);
 
 document.querySelector("#menu_toggle").addEventListener("click", function () {
-  document.querySelector(".nav_list").classList.toggle("show");
+  nav_bar.classList.toggle("show");
 });
 
+nav_bar.addEventListener("click", function (){
+ nav_bar.classList.toggle("show");
+});
 console.log("ok");
-   
+ 
